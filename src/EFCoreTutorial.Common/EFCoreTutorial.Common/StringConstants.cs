4c2a37907829f3a65b7a920857b4b93ca950de5d
Emre <eemmrree0@outlook.com>
2021-10-18 14:53:58 +0300

    DbConnectionString tanımlandı.

---
+++ src/EFCoreTutorial.Common/EFCoreTutorial.Common/StringConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreTutorial.Common
+{
+    public static class StringConstants
+    {
+        public static string DbConnectionString { get; } = "Data Source=DESKTOP-H6JU5SG\\SQLEXPRESS;Initial Catalog=efcore;Integrated Security=True";
+    }
+}
